90a168db147f01dd2a23836a41d8bcc15b8e493f
Johannes Kiechle <johannes.kiechle@tum.de>
2025-05-20 21:02:23 +0200

    Refactor train_RADIOMICS_CV.py to streamline imports, enhance classifier training logic, and improve metric logging for model evaluation

--- train_RADIOMICS_CV.py
+++ train_RADIOMICS_CV.py
@@ -1,30 +1,19 @@
-import os
-import csv
-from radiomics import featureextractor
-import SimpleITK as sitk
-import pandas as pd
-from glob import glob
-from tqdm import tqdm
-
-import pandas as pd
-import numpy as np
-import mlflow
-import torch
-
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
-from sklearn.model_selection import StratifiedKFold, GridSearchCV, cross_val_score, cross_validate
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import roc_auc_score, matthews_corrcoef
-from sklearn.model_selection import GridSearchCV, cross_val_score, KFold
-from sklearn.datasets import make_classification
+from sklearn.metrics import balanced_accuracy_score, f1_score, matthews_corrcoef, roc_auc_score
+import pandas as pd
+import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from sklearn.ensemble import RandomForestClassifier
-from utils import create_cv_splits
-
+import torch
+from glob import glob
+from radiomics import featureextractor
+import os
+from tqdm import tqdm
+import mlflow
 
 def extract_radiomics_features(task, params_path=None):
 
@@ -95,56 +84,65 @@ def extract_radiomics_features(task, params_path=None):
         df.to_csv(output_csv_path)
         print(f"\nRadiomics feature matrix saved to: {output_csv_path}")
 
-def train_classifier(task, model_type="rf", score="roc_auc"):
 
-    match task:
-        case "sarcoma_t1_grading_binary":
-            radiomics_csv_path = f"./radiomics_cache_dir/radiomics_features_{task}.csv"
-            assert os.path.exists(radiomics_csv_path)
-            df = pd.read_csv(radiomics_csv_path)
+def train_classifier(task, model_type, score="roc_auc"):
+
+    radiomics_csv_path = f"/home/johannes/Data/SSD_1.9TB/MultiViewGCN/radiomics_cache_dir/radiomics_features_{task}.csv"
+    df = pd.read_csv(radiomics_csv_path)
+    df_metadata = pd.read_csv("/home/johannes/Data/SSD_1.9TB/MultiViewGCN/data/sarcoma/patient_metadata.csv")
+
+    subjects = df["SubjectID"]
+    subjects = [subject[:6] if subject.startswith("Sar") else subject[:4] for subject in subjects]
+
+    labels = []
+    for subject in subjects:
+        grading = df_metadata[df_metadata["ID"] == subject].Grading.item()
+        labels.append(0 if grading == 1 else 1)
+
+    df["Label"] = labels
+
+    X = df.iloc[:, 38:-1].values
+    y = df.iloc[:, -1].values
+
+    # cv_outer = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+    auroc_list = list()
+    mcc_list = list()
+    f1_list = list()
+    bacc_list = list()
+
+    for fold in range(5):
+
+        print(f"Fold {fold}")   
+
+        folds_dict = torch.load(f"/home/johannes/Data/SSD_1.9TB/MultiViewGCN/data/sarcoma/{task}_folds.pt", weights_only=False)
+        train_subjects = folds_dict[fold]["train_subjects"]
+        test_subjects = folds_dict[fold]["test_subjects"]
+
+        if task == "sarcoma_t1_grading_binary":
             df["SubjectID"] = df["SubjectID"].str.replace("T1", "", regex=True)
             df["SubjectID"] = df["SubjectID"].str.replace("_updated_T1", "", regex=True)
             df["SubjectID"] = df["SubjectID"].str.replace("_updated", "", regex=True)
-            create_cv_splits(task=task)
-            folds_dict = torch.load(f"./data/sarcoma/{task}_folds.pt", weights_only=False)
-        
-        case "sarcoma_t2_grading_binary":
-            radiomics_csv_path = f"./radiomics_cache_dir/radiomics_features_{task}.csv"
-            assert os.path.exists(radiomics_csv_path)
-            df = pd.read_csv(radiomics_csv_path)
-            
+
+        elif task == "sarcoma_t2_grading_binary":
             df["SubjectID"] = df["SubjectID"].str.replace("STIR_ax", "", regex=True)
             df["SubjectID"] = df["SubjectID"].str.replace("STIR_sag", "", regex=True)
             df["SubjectID"] = df["SubjectID"].str.replace("_updatedSTIR", "", regex=True)
             df["SubjectID"] = df["SubjectID"].str.replace("STIR", "", regex=True)
-            folds_dict = torch.load("./data/sarcoma/sarcoma_t2_grading_binary_folds.pt")
-
-        case _:
-            raise ValueError("Given task is unkonwn!")
-    
-    outer_results_auroc = []
-    outer_results_mcc = []
-    for current_fold in range(5):
-
-        print(f"\nFold {current_fold}")
-
-        train_subjects = folds_dict[current_fold]["train_subjects"]
-        train_labels = folds_dict[current_fold]["train_labels"]
-        test_subjects = folds_dict[current_fold]["test_subjects"]
-        test_labels = folds_dict[current_fold]["test_labels"]
 
         train_df = df[df['SubjectID'].isin(train_subjects)]
-        X_train = train_df.iloc[:, 38:].values
-        y_train = np.array(train_labels)
-
         test_df = df[df['SubjectID'].isin(test_subjects)]
-        X_test = test_df.iloc[:, 38:].values
-        y_test = np.array(test_labels)        
+
+        X_train = train_df.iloc[:, 38:-1].values
+        y_train = np.array(train_df["Label"])
+        X_test = test_df.iloc[:, 38:-1].values
+        y_test = np.array(test_df["Label"])
+
+        cv_inner = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
         total_features = X_train.shape[1]
         n_components_list = [int(total_features * i) for i in np.arange(0.1, 1.1, 0.1)]    
-
-        # ---- DEFINE MODEL PIPELINES ----
+        
         if model_type == "rf":
             model = RandomForestClassifier(random_state=42)
             param_grid = {
@@ -152,6 +150,7 @@ def train_classifier(task, model_type="rf", score="roc_auc"):
                 "classifier__n_estimators": [50, 100, 250],
                 "classifier__max_depth": [None, 5, 10, 20]
             }
+
         elif model_type == "svm":
             model = SVC(probability=True, random_state=42)
             param_grid = {
@@ -159,50 +158,55 @@ def train_classifier(task, model_type="rf", score="roc_auc"):
                 "classifier__C": [0.01, 0.1, 1, 10, 100],
                 "classifier__kernel": ["linear", "rbf"]
             }
+
         else:
             raise ValueError("Invalid model. Choose 'rf' or 'svm'.")
 
         pipeline = Pipeline(steps=[
-        ('scaler', StandardScaler()),
-        ('pca', PCA()),
-        ('classifier', model)
+            ('scaler', StandardScaler()),
+            ('pca', PCA()),
+            ('classifier', model)
         ])
-
-        cv_inner = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-        search = GridSearchCV(pipeline, param_grid, scoring=score, cv=cv_inner, refit=True, n_jobs=-1)
         
-        # execute search
-        result = search.fit(X_train, y_train)
+        search = GridSearchCV(pipeline, param_grid, scoring=score, cv=cv_inner, refit=True)   
+        result = search.fit(X_train, y_train)    
+        best_model = result.best_estimator_   
         
-        # get the best performing model fit on the whole training set
-        best_model = result.best_estimator_
-        
-        # evaluate model on the hold out dataset
-        y_pred = best_model.predict(X_test)
+        yhat = best_model.predict(X_test)
         y_score  = best_model.predict_proba(X_test)[:, -1]
         
-        # evaluate the model
+        bacc = balanced_accuracy_score(y_test, yhat)
         auroc = roc_auc_score(y_true=y_test, y_score=y_score)
-        mcc = matthews_corrcoef(y_true=y_test, y_pred=y_pred)
+        mcc = matthews_corrcoef(y_test, yhat)
+        f1 = f1_score(y_test, yhat)
+
+        # Log metrics to MLflow
+        mlflow.log_metric("test_bacc", bacc)
+        mlflow.log_metric("test_auroc", auroc)   
+        mlflow.log_metric("test_mcc", mcc)
+        mlflow.log_metric("test_f1", f1)
+        mlflow.log_param("architecture", model_type)        
+        mlflow.log_param("fold", fold)
+
+        bacc_list.append(bacc)
+        auroc_list.append(auroc)
+        mcc_list.append(mcc)
+        f1_list.append(f1)
         
-        # store the result
-        outer_results_auroc.append(auroc)
-        outer_results_mcc.append(mcc)
-    
     # summarize the estimated performance of the model
-    print(outer_results_auroc)
-    print(f"AUROC | mean: {np.mean(outer_results_auroc)} | std: {np.std(outer_results_auroc)}")
-    print(outer_results_mcc)
-    print(f"MCC   | mean: {np.mean(outer_results_mcc)} | std: {np.std(outer_results_mcc)}")    
+    print('BACC: %.3f (%.3f)' % (np.mean(bacc_list), np.std(bacc_list)))
+    print('AUROC: %.3f (%.3f)' % (np.mean(auroc_list), np.std(auroc_list)))
+    print('MCC: %.3f (%.3f)' % (np.mean(mcc_list), np.std(mcc_list)))
+    print('F1: %.3f (%.3f)' % (np.mean(f1_list), np.std(f1_list)))
+
 
 if __name__ == "__main__":
 
-    # for task in ["sarcoma_t1_grading_binary", "sarcoma_t2_grading_binary"]:
-    for task in ["sarcoma_t1_grading_binary"]:
-        # for model in ["rf", "svm"]:
-        for model in ["rf"]:
+    for task in ["sarcoma_t2_grading_binary", "sarcoma_t1_grading_binary"]:
+        for model in ["rf", "svm"]:
             mlflow.set_experiment(task)
             mlflow.start_run()            
             extract_radiomics_features(task=task, params_path="./pyradiomics_mri_params.yaml")
             train_classifier(task=task, model_type=model, score="roc_auc")
             mlflow.end_run()
+    
